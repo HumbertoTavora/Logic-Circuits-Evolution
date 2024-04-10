57a9cb3bc0a32a081a2d0a6c7afa1e946b40a8cd
HumbertoTavora <humbertocctg@gmail.com>
2024-04-10 18:39:00 -0300

    Update python module implementation

--- Biblioteca/VLCDA/Logic_Circuits_Evolution.py
+++ Biblioteca/VLCDA/Logic_Circuits_Evolution.py
@@ -3,7 +3,47 @@ import random
 import datetime
 import bisect
 import matplotlib.pyplot as plt
+import numpy as np
+from multiprocessing.sharedctypes import copy
+from quopri import decodestring
 
+PG_collection = {}
+
+x = "2-3 2-0 0-0 2-5 5-1 8-6 1-3 9-1 4-2 11-10 4-11 5-0 4-14 10-8 3-9 15-7 14-14 13-17 8-3 11-3 11-0 21-13 3-1 26-25 5-5 25-8 6-13 0-4 8-10 32-16 23-16 15-17 23-26 34-26 19-26 7-37 30-24 23-5 16-12 22-37 31-15 29-27 27-34 26-43 43-9 23-34 3-16 47-0 27-3 5-52 44-45 19-47 16-32 41-14 9-54 39-38 31-44 23-10 50-42 59-55"
+lx = x.split(" ")
+PG_collection["PG1"] = lx
+
+x = "1-2 4-2 1-5 0-0 7-6 8-8 5-6 8-3 3-7 4-10 3-11 2-6 7-9 0-11 6-1 4-10 13-19 18-11 10-10 6-21 17-17 7-13 12-17 6-20 10-0 20-8 19-3 26-20 5-0 9-21 14-8 29-26 22-28 34-18 30-31 6-7 17-35 40-13 14-39 30-28 38-11 40-3 16-29 36-28 40-2 42-17 12-43 15-8 22-9 23-9 49-6 0-44 3-47 13-35 38-26 58-20 42-47 34-29 11-10 59-41"
+lx = x.split(" ")
+PG_collection["PG2"] = lx
+
+x = "2-0 2-3 3-5 5-6 1-0 2-5 6-9 2-10 0-8 8-9 1-8 14-12 5-10 5-6 2-12 17-9 13-16 6-15 17-7 22-7 16-20 11-18 22-7 11-12 13-3 18-13 24-15 5-28 21-16 7-8 6-14 10-5 0-18 21-32 11-11 3-28 8-2 2-11 11-5 16-5 29-39 13-6 29-22 4-37 29-11 4-48 24-44 10-2 51-35 32-30 8-46 22-0 53-23 42-35 31-1 4-32 42-39 5-11 48-38 53-26"
+lx = x.split(" ")
+PG_collection["PG3"] = lx
+
+x = "2-0 2-0 3-3 1-6 7-1 2-5 3-8 5-9 8-1 6-10 1-6 12-10 1-7 10-5 3-14 4-7 0-5 16-19 11-17 20-3 3-23 8-16 0-9 25-12 8-6 9-20 15-29 24-18 27-18 4-27 29-29 15-30 11-34 12-33 36-14 27-25 23-33 25-25 2-39 30-29 6-19 12-10 23-19 29-39 14-20 13-32 15-28 2-9 12-14 43-17 41-47 36-42 22-29 28-26 40-2 57-2 3-10 5-60 22-14 43-35"
+lx = x.split(" ")
+PG_collection["PG4"] = lx
+
+x = "1-3 3-1 1-5 3-3 1-2 2-6 2-4 2-2 6-8 1-10 11-5 5-3 0-11 15-15 7-6 1-6 8-1 2-15 18-5 1-3 6-15 13-7 15-22 11-21 24-27 25-21 2-15 0-22 26-0 25-4 22-31 4-29 24-30 34-32 23-2 0-3 23-10 28-8 41-28 39-5 17-29 4-11 28-37 32-7 13-20 44-43 39-46 50-32 42-41 44-13 3-5 7-11 10-20 56-51 19-0 39-53 2-37 1-28 11-46 60-62"
+lx = x.split(" ")
+PG_collection["PG5"] = lx
+
+x = "3-0 0-1 5-1 4-6 5-0 4-2 6-7 5-3 7-2 11-8 5-10 3-9 6-3 3-0 17-3 7-12 12-7 6-9 11-3 18-8 2-12 24-20 17-22 9-23 25-23 13-25 13-0 10-16 21-29 29-25 21-28 20-8 15-30 29-2 20-15 30-11 30-31 38-4 34-41 1-34 38-25 13-1 45-19 23-20 27-25 7-4 6-15 29-37 34-11 1-19 43-37 29-13 35-5 5-36 35-30 53-15 59-48 13-44 37-20 55-33"
+lx = x.split(" ")
+PG_collection["PG6"] = lx
+
+x = "2-0 4-4 0-3 6-0 6-3 8-7 2-9 10-9 1-1 12-10 4-5 6-12 3-8 2-10 14-15 8-14 11-17 12-12 1-4 15-20 23-4 2-16 7-17 23-21 16-23 6-28 9-26 3-10 27-25 32-32 22-31 34-18 33-5 1-10 27-26 12-20 2-8 2-29 10-40 39-0 17-4 23-19 14-19 0-24 11-4 6-48 43-4 12-47 47-7 17-9 27-39 30-5 10-39 47-33 9-11 52-2 53-3 5-21 48-30 39-27"
+lx = x.split(" ")
+PG_collection["PG7"] = lx
+
+x = "2-2 0-3 5-0 4-6 1-2 1-4 3-5 10-6 3-4 2-4 12-2 12-4 2-8 9-16 10-17 18-2 13-17 1-20 19-17 13-1 2-23 1-0 17-0 22-22 11-27 7-27 13-18 9-4 7-7 30-22 26-32 25-27 31-12 21-6 31-2 15-21 1-28 4-10 11-20 6-30 39-27 5-12 1-11 16-13 18-6 43-23 27-24 20-46 32-2 47-6 37-45 44-10 5-43 41-44 56-13 26-1 8-29 44-54 55-34 42-43"
+lx = x.split(" ")
+PG_collection["PG8"] = lx
+
+x = "0-1 1-4 3-1 6-3 0-0 4-0 1-1 9-5 4-8 3-11 10-6 7-5 8-9 2-5 1-4 13-3 13-12 18-1 11-13 0-10 22-19 15-16 3-23 2-24 22-14 22-21 14-29 29-6 27-2 8-16 33-6 28-6 23-3 5-21 7-37 30-30 39-5 9-39 30-10 27-2 15-32 44-11 22-41 33-31 6-31 24-29 14-25 29-6 35-10 23-41 9-36 12-29 27-24 47-16 8-42 47-24 16-16 2-41 29-58 56-32"
+lx = x.split(" ")
+PG_collection["PG9"] = lx
 
 class Genoma:
     def __init__(self, numberOfGenes = 60, nInputs = 4,nOutputs = 1, rateMutation = 0.10):
@@ -200,7 +240,125 @@ class Genoma:
           CartesianProduct = [a+[b] for a in CartesianProduct for b in iterable]
         return CartesianProduct
 
-    def redundantDegenerationCalcFitness(self):
+    def redundantCalcFitness(self,trys,GPINAND_ID):
+
+        listGenome = []
+        x = "2-3 2-0 0-0 2-5 5-1 8-6 1-3 9-1 4-2 11-10 4-11 5-0 4-14 10-8 3-9 15-7 14-14 13-17 8-3 11-3 11-0 21-13 3-1 26-25 5-5 25-8 6-13 0-4 8-10 32-16 23-16 15-17 23-26 34-26 19-26 7-37 30-24 23-5 16-12 22-37 31-15 29-27 27-34 26-43 43-9 23-34 3-16 47-0 27-3 5-52 44-45 19-47 16-32 41-14 9-54 39-38 31-44 23-10 50-42 59-55"
+        lx = x.split(" ")
+        listGenome.append(lx)
+
+        x = "1-2 4-2 1-5 0-0 7-6 8-8 5-6 8-3 3-7 4-10 3-11 2-6 7-9 0-11 6-1 4-10 13-19 18-11 10-10 6-21 17-17 7-13 12-17 6-20 10-0 20-8 19-3 26-20 5-0 9-21 14-8 29-26 22-28 34-18 30-31 6-7 17-35 40-13 14-39 30-28 38-11 40-3 16-29 36-28 40-2 42-17 12-43 15-8 22-9 23-9 49-6 0-44 3-47 13-35 38-26 58-20 42-47 34-29 11-10 59-41"
+        lx = x.split(" ")
+        listGenome.append(lx)
+
+        x = "2-0 2-3 3-5 5-6 1-0 2-5 6-9 2-10 0-8 8-9 1-8 14-12 5-10 5-6 2-12 17-9 13-16 6-15 17-7 22-7 16-20 11-18 22-7 11-12 13-3 18-13 24-15 5-28 21-16 7-8 6-14 10-5 0-18 21-32 11-11 3-28 8-2 2-11 11-5 16-5 29-39 13-6 29-22 4-37 29-11 4-48 24-44 10-2 51-35 32-30 8-46 22-0 53-23 42-35 31-1 4-32 42-39 5-11 48-38 53-26"
+        lx = x.split(" ")
+        listGenome.append(lx)
+
+        x = "2-0 2-0 3-3 1-6 7-1 2-5 3-8 5-9 8-1 6-10 1-6 12-10 1-7 10-5 3-14 4-7 0-5 16-19 11-17 20-3 3-23 8-16 0-9 25-12 8-6 9-20 15-29 24-18 27-18 4-27 29-29 15-30 11-34 12-33 36-14 27-25 23-33 25-25 2-39 30-29 6-19 12-10 23-19 29-39 14-20 13-32 15-28 2-9 12-14 43-17 41-47 36-42 22-29 28-26 40-2 57-2 3-10 5-60 22-14 43-35"
+        lx = x.split(" ")
+        listGenome.append(lx)
+
+        x = "1-3 3-1 1-5 3-3 1-2 2-6 2-4 2-2 6-8 1-10 11-5 5-3 0-11 15-15 7-6 1-6 8-1 2-15 18-5 1-3 6-15 13-7 15-22 11-21 24-27 25-21 2-15 0-22 26-0 25-4 22-31 4-29 24-30 34-32 23-2 0-3 23-10 28-8 41-28 39-5 17-29 4-11 28-37 32-7 13-20 44-43 39-46 50-32 42-41 44-13 3-5 7-11 10-20 56-51 19-0 39-53 2-37 1-28 11-46 60-62"
+        lx = x.split(" ")
+        listGenome.append(lx)
+
+        x = "3-0 0-1 5-1 4-6 5-0 4-2 6-7 5-3 7-2 11-8 5-10 3-9 6-3 3-0 17-3 7-12 12-7 6-9 11-3 18-8 2-12 24-20 17-22 9-23 25-23 13-25 13-0 10-16 21-29 29-25 21-28 20-8 15-30 29-2 20-15 30-11 30-31 38-4 34-41 1-34 38-25 13-1 45-19 23-20 27-25 7-4 6-15 29-37 34-11 1-19 43-37 29-13 35-5 5-36 35-30 53-15 59-48 13-44 37-20 55-33"
+        lx = x.split(" ")
+        listGenome.append(lx)
+
+        x = "2-0 4-4 0-3 6-0 6-3 8-7 2-9 10-9 1-1 12-10 4-5 6-12 3-8 2-10 14-15 8-14 11-17 12-12 1-4 15-20 23-4 2-16 7-17 23-21 16-23 6-28 9-26 3-10 27-25 32-32 22-31 34-18 33-5 1-10 27-26 12-20 2-8 2-29 10-40 39-0 17-4 23-19 14-19 0-24 11-4 6-48 43-4 12-47 47-7 17-9 27-39 30-5 10-39 47-33 9-11 52-2 53-3 5-21 48-30 39-27"
+        lx = x.split(" ")
+        listGenome.append(lx)
+
+        x = "2-2 0-3 5-0 4-6 1-2 1-4 3-5 10-6 3-4 2-4 12-2 12-4 2-8 9-16 10-17 18-2 13-17 1-20 19-17 13-1 2-23 1-0 17-0 22-22 11-27 7-27 13-18 9-4 7-7 30-22 26-32 25-27 31-12 21-6 31-2 15-21 1-28 4-10 11-20 6-30 39-27 5-12 1-11 16-13 18-6 43-23 27-24 20-46 32-2 47-6 37-45 44-10 5-43 41-44 56-13 26-1 8-29 44-54 55-34 42-43"
+        lx = x.split(" ")
+        listGenome.append(lx)
+
+        x = "0-1 1-4 3-1 6-3 0-0 4-0 1-1 9-5 4-8 3-11 10-6 7-5 8-9 2-5 1-4 13-3 13-12 18-1 11-13 0-10 22-19 15-16 3-23 2-24 22-14 22-21 14-29 29-6 27-2 8-16 33-6 28-6 23-3 5-21 7-37 30-30 39-5 9-39 30-10 27-2 15-32 44-11 22-41 33-31 6-31 24-29 14-25 29-6 35-10 23-41 9-36 12-29 27-24 47-16 8-42 47-24 16-16 2-41 29-58 56-32"
+        lx = x.split(" ")
+        listGenome.append(lx)
+        
+        listGenomeRedundant = []
+        for i in range(0,9):
+          listGenomeRedundant.append(listGenome[GPINAND_ID-1])
+        
+        print("GPINAND_ID:",GPINAND_ID,listGenomeRedundant[GPINAND_ID-1])
+        
+        listFitness = []
+        for i in range(0,trys):
+          matrixOuts = []
+          l = []
+          dic =  dict() 
+
+          for i in range(0,self.nInputs):
+              subL = [0,1]
+              l.append(subL)
+
+          TrueTable = self.getCartesianProduct(l)
+
+          for i in range(0,self.possiblesOutputs):
+              ithTrueTable = TrueTable[i]
+              for input in range(0,self.nInputs):
+                  sinput = str(input)
+                  dic[sinput] = ithTrueTable[input]
+              
+              listOuts = []
+              for genoma in listGenomeRedundant:
+                indexOut = self.nInputs
+                for element in genoma:
+                    if(True):
+                      elements = element.split("-")
+                      in1 = elements[0]
+                      in2 = elements[1]
+                      out = self.NAND_defeitos(dic[in1], dic[in2])
+                      sindexOut = str(indexOut)
+                      dic[sindexOut] = out
+                    indexOut+=1
+                
+                lGPINAND = []
+                for m in range(0,self.nInputs):
+                  sm = str(m)
+                  value = dic[sm]
+                  lGPINAND.append(value)
+                listOuts.append(dic[sindexOut])
+
+              matrixOuts.append(listOuts)
+          
+#          print(dic)
+
+          outTrue = []                      #
+          for i in TrueTable:               # Get the truTable inputs values
+            outTrue.append(int(self.gpiNand(i))) #
+
+#          for i in range(0,self.possiblesOutputs):
+#            print("Para:", TrueTable[i], " as saidas foram:")
+#            print(matrixOuts[i])
+          
+
+          result = []
+          for outsList in matrixOuts:
+            if(outsList.count(1) > outsList.count(0)):
+              result.append(1)
+            else:
+              result.append(0)           
+          
+#          print("\n\nA lista dos outputs CORRETOS é:\n", outTrue)
+#          print("\nA lista dos outputs dos mais votados é:\n",result,"\n")
+          c = 0
+          for i in range(0,self.possiblesOutputs):
+            if(result[i] == outTrue[i]):
+              c+=1
+          fitness = c/self.possiblesOutputs
+          listFitness.append(fitness)
+        
+        c = 0
+        for fitness in listFitness:
+          c += fitness
+        print(listFitness)
+        return c/trys
+
+    def redundantDegenerationCalcFitness(self,trys):
         listGenome = []
         x = "2-3 2-0 0-0 2-5 5-1 8-6 1-3 9-1 4-2 11-10 4-11 5-0 4-14 10-8 3-9 15-7 14-14 13-17 8-3 11-3 11-0 21-13 3-1 26-25 5-5 25-8 6-13 0-4 8-10 32-16 23-16 15-17 23-26 34-26 19-26 7-37 30-24 23-5 16-12 22-37 31-15 29-27 27-34 26-43 43-9 23-34 3-16 47-0 27-3 5-52 44-45 19-47 16-32 41-14 9-54 39-38 31-44 23-10 50-42 59-55"
         lx = x.split(" ")
@@ -240,10 +398,8 @@ class Genoma:
         listGenomeRedundant = listGenome
         
         listFitness = []
-        trys = 10000
         for i in range(0,trys):
           matrixOuts = []
-          fitnessCounter = 0
           l = []
           dic =  dict() 
 
@@ -262,17 +418,15 @@ class Genoma:
               listOuts = []
               for genoma in listGenomeRedundant:
                 indexOut = self.nInputs
-                position = 0
                 for element in genoma:
                     if(True):
                       elements = element.split("-")
                       in1 = elements[0]
                       in2 = elements[1]
-                      out = self.NAND(dic[in1], dic[in2])
+                      out = self.NAND_defeitos(dic[in1], dic[in2])
                       sindexOut = str(indexOut)
                       dic[sindexOut] = out
                     indexOut+=1
-                    position+=1
                 
                 lGPINAND = []
                 for m in range(0,self.nInputs):
@@ -283,15 +437,15 @@ class Genoma:
 
               matrixOuts.append(listOuts)
           
-          #print(dic)
+#          print(dic)
 
           outTrue = []                      #
           for i in TrueTable:               # Get the truTable inputs values
-            outTrue.append(self.gpiNand(i)) #
+            outTrue.append(int(self.gpiNand(i))) #
 
-          #for i in range(0,self.possiblesOutputs):
-          #  print("Para:", TrueTable[i], " as saidas foram:")
-          #  print(matrixOuts[i])
+#          for i in range(0,self.possiblesOutputs):
+#            print("Para:", TrueTable[i], " as saidas foram:")
+#            print(matrixOuts[i])
           
 
           result = []
@@ -301,21 +455,20 @@ class Genoma:
             else:
               result.append(0)           
           
-          #print("\n\nA lista dos outputs CORRETOS é:\n", outTrue)
-          #print("\nA lista dos outputs dos mais votados é:\n",result)
+#          print("\n\nA lista dos outputs CORRETOS é:\n", outTrue)
+#          print("\nA lista dos outputs dos mais votados é:\n",result,"\n")
           c = 0
           for i in range(0,self.possiblesOutputs):
             if(result[i] == outTrue[i]):
               c+=1
           fitness = c/self.possiblesOutputs
-          #print(fitness,"\n\n\n\n\n")
           listFitness.append(fitness)
         
         c = 0
         for fitness in listFitness:
           c += fitness
         print(listFitness)
-        print("\n\n\nFitness final: ", c/trys)     
+        return c/trys
 
     def calculateFitness(self,logicFuncion):
         self.identify_deadGenes()
@@ -342,7 +495,7 @@ class Genoma:
                   elements = element.split("-")
                   in1 = elements[0]
                   in2 = elements[1]
-                  out = self.NAND(dic[in1], dic[in2])
+                  out = self.NAND_defeitos(dic[in1], dic[in2])
                   #print(in1,in2,out)
                   sindexOut = str(indexOut)
                   dic[sindexOut] = out
@@ -373,7 +526,7 @@ class Genoma:
         noise = random.uniform(-self.Stochasticity,self.Stochasticity)
         noise = round(noise,4)
         self.noiseFitness = self.fitness + noise
-        
+
     def copyGene(self, destiny):
         destiny.genotipo = self.genotipo.copy()
         destiny.copyGenotipo = self.copyGenotipo.copy()
@@ -424,7 +577,7 @@ class GeneticAlgorithm():
         self.totalGeneration = 0
         self.countGeneration = 0
         self.maxGeneration = maxGeneration
-        self.histogram = []
+        self.fitnessList = []
 
     def display(self, guess, fitness, noiseFitness, totalGeneration):
         sguess = ' '.join(guess)
@@ -432,24 +585,40 @@ class GeneticAlgorithm():
 
         print("{0}\t {1}\t {2}\t Geração: {3} Tempo: {4}\n ".format(sguess, fitness, round(noiseFitness, 4), self.totalGeneration, str(timeDiff), self.totalGeneration))
 
-    def makeHistgram(self, childFitness):                                                  # make the histogram list                                                         
-        bisect.insort(self.histogram,str(childFitness))                                # Using the bisect library, insert the fitness garanting the sorting
+    def addToFitnessList(self, childFitness):                                           # Using the bisect library, insert the fitness garanting the sorting                                                       
+        bisect.insort(self.fitnessList,str(childFitness))                                
     
-    def showHistogram(self):
-        y = self.histogram
+    def showBarPlot(self, genome_id, samplingLen):
+        arr = self.fitnessList
+        elements_count = {}
+        yNormalization = samplingLen/10
 
-        x=[]
-        n = len(y)
-        for fitness in y:
-            x.append(float(fitness)) 
+        for element in arr:
+            if element in elements_count:
+                elements_count[element] += 1
+            else:
+                elements_count[element] = 1
 
-        print(len(x))
+        for key, value in elements_count.items():
+            print(f"{key}: {value}")
+            
+        values = list(elements_count.keys())
+        frequency = list(elements_count.values())
+        frequency =  [countFitness / yNormalization for countFitness in frequency]
+        
+        fig = plt.figure(figsize = (10, 5))
+        
+        # creating the bar plot
+        plt.bar(values, frequency, color='plum', label='F2')
+        plt.plot(values, frequency, marker='o', color='purple', label='Out')
 
-        plt.hist(x, bins = 12)
+
         plt.ylabel('Occurrency')
-        plt.xlabel('Fitness')
-        plt.title("Histogram")
-        plt.savefig('fitnessHistogram1.svg', dpi=300, format='svg')
+        plt.xlabel('Fitness', fontsize=18)
+        plt.ylabel('Frequency (' + str(int(yNormalization)) +')', fontsize=18)
+        plt.grid(axis='y')
+        
+        plt.savefig(genome_id + '.eps', dpi=300, format='eps')
         plt.show()
 
     def getBestGenomeWithSize(self, listChild):
@@ -603,7 +772,6 @@ class GeneticAlgorithm():
         timeDiff = datetime.datetime.now() - self.startTime
         print("The end in: ",str(timeDiff))
 
-
 def gpinand(l,nOutputs = 1):
   count = 0
   n = len(l)
@@ -616,24 +784,21 @@ def gpinand(l,nOutputs = 1):
   else: 
       return "1"
 
-def fullAdderNand(l,nOutputs):
-      middle = int(len(l)/2) + 1
-      input1 = [str(x) for x in l[1:middle]]
-      input2 = [str(x) for x in l[middle:]]
-      cIn = [str(l[0])]
-      input1s = ''.join(input1) 
-      input2s = ''.join(input2)
-      cIns = ''.join(cIn)
-      sum_bits = bin(int(input1s, 2) + int(input2s, 2) + int(cIns,2))[2:]
-      sum_bits_string = sum_bits.zfill(nOutputs)
-
-      return sum_bits_string
-
-nGenes = 30
-nOutputs = 1
-nInputs = 4
-genome = Genoma(nGenes,nInputs,nOutputs)
-geneticAlgorithm = GeneticAlgorithm()
-geneticAlgorithm.evolution(genome,gpinand)
-print(genome.fitness)
-print(geneticAlgorithm.histogram)
+def fullAdderNand(l, nOutputs):
+    # Preparing
+    middle = int(len(l)/2) + 1
+    input1 = [str(x) for x in l[1:middle]]
+    input2 = [str(x) for x in l[middle:]]
+    cIn = [str(l[0])]
+    input1s = ''.join(input1) 
+    input2s = ''.join(input2)
+    cIns = ''.join(cIn)
+    #print(input1s, input2s, cIns)
+    # Logical Expressions
+    sum = bin(int(input1s, 2) ^ int(input2s, 2) ^ int(cIns, 2))[2:]
+    cOut = bin((int(input1s, 2) & int(input2s, 2)) | (int(input2s, 2) & int(cIns, 2)) | (int(input1s, 2) & int(cIns, 2)))[2:]
+    sum_bits =str(sum) + str(cOut) 
+    sum_bits_string = sum_bits.zfill(nOutputs)
+    #print(sum_bits_string)
+    # Returning
+    return sum_bits_string
